972fc3ab89cff9b18a63dd9783b96ec9f7b50929
py-46-Alex <90619667+py-46-Alex@users.noreply.github.com>
2023-04-07 18:21:42 +0300

    Update servr.py

--- servr.py
+++ servr.py
@@ -19,101 +19,6 @@ from sqlalchemy.future import select
 
 os.environ["SQLALCHEMY_SILENCE_UBER_WARNING"]="1"
 #
-# class HttpError(Exception):
-#     #   база для хендлера. действует по шаблону базы
-#     def __init__(self, status_code: int, description):
-#         self.status_code = status_code
-#         self.description = description
-#  # 3 то для чего создавали базу
-# @app.errorhandler(HttpError)
-# def error_handler(error: HttpError):
-#     response = jsonify({'status': 'error', 'description': error.description})
-#     response.status_code = error.status_code
-#     return response
-# #
-# class CreateAdvUser(pydantic.BaseModel):
-#     head_adv: str
-#     autor: str
-#     @pydantic.validator('autor')
-#     def validate_nikname(cls, value):
-#         if len(value) <= 3:
-#             raise ValueError("Username_is_too_SHORT")
-#         return value
-# #
-# def validate(input_data: dict, validation_model: Type[CreateAdvUser]):
-#     # засунуто в пост чтобы разрешало постить только тем у кого указан ник и
-#     # гоолва обьявления
-#     try:
-#         model_item = validation_model(**input_data)
-#         return model_item.dict(exclude_none=True)
-#         #если пришло нан то просто ничего не делай . ненадо писать нан так же.
-#     except pydantic.ValidationError as err:
-#         raise HttpError(400, err.errors())  #делает описание ошибки атвтоматичси
-# #
-
-#
-#
-# #
-# class PatchAdv(pydantic.BaseModel):
-#     head_adv: Optional[str]
-#     # опционально может быть а может и не быть автора
-#     @pydantic.validator('head_adv')
-#     def validate_nikname(cls, value):
-#         if len(value) <= 3:
-#             raise ValueError("head_adv_is_too_SHORT")
-#         return value
-# #
-# def hi():
-#     json_data = request.json
-#     print(f"{json_data=}")
-#     return jsonify({"hi WORLD": "WE HAVE NO PROBLEM"})
-#
-# #
-# def get_user(useradven_id: int, session: Session):
-#     user = session.get(Useradven, useradven_id)
-#     if user is None:
-#         return jsonify({'HET': 'TAKOrO userA'})
-#     return user
-# #
-# class UserViev(MethodView):
-#     def get(self, useradven_id: int):
-#         with Session() as session:
-#             user = get_user(useradven_id, session)
-#             return jsonify({"ID": user.id, "aBTOP" :user.id, "BPEMya": user.time_create.isoformat()})
-#     #
-#     def post(self):
-#         jsn_data = request.json
-#         # можно делать таким образом.
-#         if 'text_of_adv' not in jsn_data:
-#             raise HttpError(409, "HET_avde-texta")
-#         with Session() as session:
-#             adver = Useradven(head_adv=jsn_data.get('head_adv'),
-#                               autor=jsn_data.get('autor'))
-#             session.add(adver)
-#             try:
-#                 session.commit()
-#             except IntegrityError as er:
-#                 raise HttpError(409, "user_K_nPuMEPY_exist")
-#                 #кусок кода такой же но другим способом.
-#                 # response = jsonify({'id': 'user_K_nPuMEPY_exist'})
-#                 # response.status_code = 409
-#                 # return {"id": jsn_data.get("id")}
-#                  #
-#             return jsonify({' Объявление внезапно CO3DAHO ABTOPOM': adver.autor})
-#     #
-#     def patch(self):
-#         jsn_data = validate(request.json, PatchAdv)
-#         return jsonify({"HEDOnuCAHHO": "noka..."})
-#
-#
-#     #
-#     def delete(self, useradven_id):
-#         with Session() as session:
-#             user = get_user(useradven_id, session)
-#             session.delete(user)
-#             session.commit()
-#             return jsonify({"ID": "DELETED OK "})
-#
 
 if sys.version_info >= (3, 8) and sys.platform.lower().startswith("win"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -123,16 +28,6 @@ engine = create_async_engine(PG_DSN)
 Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 #
 Base = declarative_base()
-#
-# async def fest(request: web.Request):
-#     json_data = await request.json()
-#     headers = request.headers
-#     qs = request.query
-#     return web.json_response({
-#         "hiWORLD": "worrld HELLO!",
-#         "json": json_data,
-#         "headers": dict(headers),
-#         "qs": dict(qs)})
 #
 app = web.Application()
 #
